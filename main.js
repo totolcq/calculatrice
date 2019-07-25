1b4829041a7f39ef940b57e0eb0a0de285237ff3
Toto <thomas.2504@live.fr>
2019-07-25 14:27:38 +0200

    scnd

---
+++ main.js
@@ -0,0 +1,11 @@
+function affiche() {   
+    this.textContent();
+}
+let bouton = document.getElementsByTagName("button");
+bouton.addEventListener("click", affiche);
+
+for (let i=0; i<bouton.length; i++)
+{
+    
+}
+
