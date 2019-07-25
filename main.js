53f0271dc717899ec1d33656997603d4bf355ccb
Toto <thomas.2504@live.fr>
2019-07-25 16:13:35 +0200

    third

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function affiche() 
     {   
-        document.getElementsByTagName("button") ;
+        resultat.textContent = this.resultat;
     }
 
 let bouton = document.getElementsByTagName("button");
-
+let resultat = document.getElementsByClassName("screen");
 
 for (let i=0; i<bouton.length; i++)
 {
     bouton[i].addEventListener("click", affiche);
+    
 }
 
 
